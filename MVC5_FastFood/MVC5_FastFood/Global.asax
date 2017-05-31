8304e1c5a08c7efe0115a4c5ba1c72c50368c4ff
leone <leocr_lem@yahoo.com.br>
2017-05-31 19:31:00 -0300

    INICIAL

---
+++ MVC5_FastFood/MVC5_FastFood/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5_FastFood.MvcApplication" Language="C#" %>
---
+++ MVC5_FastFood/MVC5_FastFood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_FastFood.Startup))]
+namespace MVC5_FastFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
